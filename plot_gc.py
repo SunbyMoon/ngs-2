d2624db93dac62a7e13748a291ad0be9daf601df
Xiongyanshi <1240172230@qq.com>
2018-01-06 13:35:23 +0800

    not finish plot gc working

---
+++ plot_gc.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+# _*_ coding: utf-8 _*_
+
+import sys
+import pysam
+import matplotlib as plt
+
+
+def usage():
+    message_finish = """to plot gc-content of whole chromosome,
+with default window length 5 bases"""
+    message_doing = '''It's still developing, vim open to check coding'''
+    print(message_doing)
+
+
+def main():
+    """
+    main job
+    """
+    pass
+
+
+if __name__ == '__main__':
+    # main()
+    usage()
+    print('\n -- done! -- \n')
